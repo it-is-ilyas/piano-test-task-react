3dc832fa7b5b5b65516f214a11f1665e08802c87
Ilyas <iskhakovilyas@gmail.com>
2021-01-31 15:35:13 +0300

    change styles for root component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "views/layout/Header";
 import OrdersPage from "views/pages/OrdersPage";
 
+import "./App.scss";
+
 type AppProps = {};
 
 const App: React.FC<AppProps> = () => {
